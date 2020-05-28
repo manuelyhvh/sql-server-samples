1b80c431373341b9fb73b6f181ab992f733dc908
Bill Liang <bilia@microsoft.com>
2020-05-28 09:07:01 -0700

    yes to extension remove

--- samples/features/azure-arc/deployment/kubeadm/ubuntu-single-node-vm/cleanup-controller.sh
+++ samples/features/azure-arc/deployment/kubeadm/ubuntu-single-node-vm/cleanup-controller.sh
@@ -6,9 +6,9 @@ sudo kubeadm reset --force
 
 # Clean up azdata-cli package.
 #
-azdata extension remove --name azdata-cli-dc
-azdata extension remove --name azdata-cli-postgres
-azdata extension remove --name azdata-cli-sqlinstance
+azdata extension remove --name azdata-cli-dc --yes
+azdata extension remove --name azdata-cli-postgres --yes
+azdata extension remove --name azdata-cli-sqlinstance --yes
 unalias azdata
 unalias az
 sudo dpkg --remove --force-all azdata-cli
