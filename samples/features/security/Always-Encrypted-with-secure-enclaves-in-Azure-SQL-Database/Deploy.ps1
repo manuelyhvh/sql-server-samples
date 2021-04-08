a0389d5b645478cf3d6866879fc3f3664f702178
Pieter Vanhove <pieter.vanhove@microsoft.com>
2021-04-08 12:11:18 +0200

    Add files via upload

---
+++ samples/features/security/Always-Encrypted-with-secure-enclaves-in-Azure-SQL-Database/Deploy.ps1
@@ -0,0 +1,35 @@
+﻿######################################################################
+# Request parameters to the user that are needed for the deployment
+######################################################################
+
+$subscriptionName = Read-Host -Prompt "Enter your subscription name"
+$projectName = Read-Host -Prompt "Enter a project name that is used to generate resource names"
+$location = Read-Host -Prompt "Enter a region where you want to deploy the demo environment"
+$upn = Read-Host -Prompt "Enter your email address used to sign in to Azure"
+$adminUsername = Read-Host -Prompt "Enter the SQL Admin Username"
+$adminPassword = Read-Host -Prompt "Enter the SQL Admin Password"
+
+$adUserId = (az ad user show --id $upn --query objectId)
+$clientIP = (Invoke-WebRequest ifconfig.me/ip).Content.Trim()
+$bicepFile = "Deploy AE Demo.bicep"
+$projectName = $projectName.ToLower()
+
+######################################################################
+# Sign in to Azure
+######################################################################
+
+Connect-AzAccount
+$context = Set-AzContext -Subscription $subscriptionName
+
+######################################################################
+# Create a resource group
+######################################################################
+$resourceGroupName = "${projectName}"
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+######################################################################
+# Deploy the resources for the demo environment
+######################################################################
+az deployment group create --name DeployAEWithEnclavesDemo --template-file $bicepFile --resource-group $resourceGroupName --parameters objectId=$adUserId projectname=$projectName adminUsername=$adminUsername adminPassword=$adminPassword AADSQLAdmin=$upn clientIP=$clientIP
+
+
